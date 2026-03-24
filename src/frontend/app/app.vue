ced9073a9f63f5dc0c8f3678e4ddae89179af952
David <davidquispe2109@gmail.com>
2026-03-24 21:16:22 +0100

    Inicialització del projecte amb les seves dependecies i les seves respectives tecnologies.

---
+++ src/frontend/app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-red-500 text-xl font-bold ">Prova</h1>
+    <button class="btn btn-blue hover:bg-blue-700 rounded py-2 px-4 bg-blue-500">ve</button>    
+  </div>
+</template>
---
+++ src/realtime/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuari connectat");
+});
+
+server.listen(3001);
