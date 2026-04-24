fda24e3ca44f36ce110b21957ccf222cdafd1766
David <davidquispe2109@gmail.com>
2026-04-24 07:18:17 +0200

    deploy CD/CI

--- src/frontend/cypress.config.ts
+++ src/frontend/cypress.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.ts",
     supportFile: "cypress/support/e2e.ts",
